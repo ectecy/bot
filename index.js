7a14c1581a166f39e7ab9a571d6def9ff1cd62f4
ectecy <velocity3343@gmail.com>
2026-06-19 21:50:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -530,31 +530,34 @@ if (cmd === "unlock") {
 
 if (cmd === "hug") {
 
-            const target = message.mentions.members.first();
+    const targetUser = message.mentions.users.first();
+    if (!targetUser)
+        return message.reply("❌ Mention someone like: ,hug @user");
 
-            if (!target)
-                return message.reply("❌ Mention someone like: ,hug @user");
+    if (targetUser.id === message.author.id)
+        return message.reply("🤗 You hug yourself... wholesome.");
 
-            if (target.id === message.author.id)
-                return message.reply("🤗 You hug yourself... wholesome.");
+    const targetMember = await message.guild.members.fetch(targetUser.id).catch(() => null);
+    if (!targetMember)
+        return message.reply("❌ User not found in server.");
 
-            const gifs = [
-                "https://images-ext-1.discordapp.net/external/_cgFjM_9UiGfG4IgQRlXbKFYYmHguUWhkbZlWw9pt2s/https/nekos.best/api/v2/hug/01b8d0bb-827b-49ed-a538-c109ee5883e1.gif",
-                "https://images-ext-1.discordapp.net/external/eRYiZabajb-add7P4V4XrTJZL01or6Izr6sSd_1y7z4/https/nekos.best/api/v2/hug/c08992e3-ec9c-4cf4-88d0-6ca921d3ef64.gif",
-                "https://images-ext-1.discordapp.net/external/EmtO43xHdJSevgT2Z_o1ii9pkIWp6xx4wiG5Zok35zY/https/nekos.best/api/v2/hug/63af9168-20da-42f8-a3f3-0299743f513c.gif"
-            ];
+    const gifs = [
+        "https://images-ext-1.discordapp.net/external/_cgFjM_9UiGfG4IgQRlXbKFYYmHguUWhkbZlWw9pt2s/https/nekos.best/api/v2/hug/01b8d0bb-827b-49ed-a538-c109ee5883e1.gif",
+        "https://images-ext-1.discordapp.net/external/eRYiZabajb-add7P4V4XrTJZL01or6Izr6sSd_1y7z4/https/nekos.best/api/v2/hug/c08992e3-ec9c-4cf4-88d0-6ca921d3ef64.gif",
+        "https://images-ext-1.discordapp.net/external/EmtO43xHdJSevgT2Z_o1ii9pkIWp6xx4wiG5Zok35zY/https/nekos.best/api/v2/hug/63af9168-20da-42f8-a3f3-0299743f513c.gif"
+    ];
 
-            const gif = gifs[Math.floor(Math.random() * gifs.length)];
+    const gif = gifs[Math.floor(Math.random() * gifs.length)];
 
-            return message.channel.send({
-                embeds: [
-                    new EmbedBuilder()
-                        .setColor("Pink")
-                        .setDescription(`🤗 ${message.author} hugs ${target.user}`)
-                        .setImage(gif)
-                ]
-            });
-        }
+    return message.channel.send({
+        embeds: [
+            new EmbedBuilder()
+                .setColor("Pink")
+                .setDescription(`🤗 ${message.author} hugs ${targetMember.user}`)
+                .setImage(gif)
+        ]
+    });
+}
         
 if (cmd === "kiss") {
     const target = message.mentions.members.first();
