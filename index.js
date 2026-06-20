c3a76f07233bc03c8e7c0e76fb4e0f9d26775e29
ectecy <velocity3343@gmail.com>
2026-06-19 20:47:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -535,10 +535,9 @@ if (cmd === "hug") {
         return message.reply("❌ Mention someone like: ,hug @user");
 
     const gifs = [
-        "https://images-ext-1.discordapp.net/external/eRYiZabajb-add7P4V4XrTJZL01or6Izr6sSd_1y7z4/https/nekos.best/api/v2/hug/c08992e3-ec9c-4cf4-88d0-6ca921d3ef64.gif",
-        "https://images-ext-1.discordapp.net/external/37qvtvvQ08qWOfXeLbWtKg7fWEKmUd9So4RYWB2_1sA/https/nekos.best/api/v2/hug/efdcc435-0402-4703-bc3b-cab18ee653bb.gif",
-        "https://images-ext-1.discordapp.net/external/EmtO43xHdJSevgT2Z_o1ii9pkIWp6xx4wiG5Zok35zY/https/nekos.best/api/v2/hug/63af9168-20da-42f8-a3f3-0299743f513c.gif",
-        "https://images-ext-1.discordapp.net/external/_cgFjM_9UiGfG4IgQRlXbKFYYmHguUWhkbZlWw9pt2s/https/nekos.best/api/v2/hug/01b8d0bb-827b-49ed-a538-c109ee5883e1.gif"
+        "https://media.tenor.com/4Y9x9qX1g2AAAAAC/anime-hug.gif",
+        "https://media.tenor.com/8Hc1v3Y5yJgAAAAC/hug-anime.gif",
+        "https://media.tenor.com/1l3xQwZy2m8AAAAC/cute-hug.gif"
     ];
 
     const gif = gifs[Math.floor(Math.random() * gifs.length)];
@@ -560,10 +559,9 @@ if (cmd === "kiss") {
         return message.reply("❌ Mention someone like: ,kiss @user");
 
     const gifs = [
-        "https://images-ext-1.discordapp.net/external/0m4Oy_Ijxw91S72KgCtOk_izKq3QgYccyOFt3SxmVX8/https/nekos.best/api/v2/kiss/ec4ea38c-2433-478b-83fd-897e7c750111.gif",
-        "https://images-ext-1.discordapp.net/external/kwUZ9Gy_V9BQbjMyurp0DiC8F8E8ncBzgzcgQhOJpcw/https/nekos.best/api/v2/kiss/a24dd374-8424-4015-8d99-a0187c6debcd.gif",
-        "https://images-ext-1.discordapp.net/external/zqKWFXh7-Y2VpfyAFnnUSrl2Vc4PvrYxggRDS9XThuw/https/nekos.best/api/v2/kiss/265e12ad-eaff-4309-bcba-e7ba8173e580.gif",
-        "https://images-ext-1.discordapp.net/external/_jvUgcTCOS0-Hy6i5QKqekYCtayZRhL_a4L8yxaM4Q8/https/nekos.best/api/v2/kiss/7d248c83-2c91-480d-b7a2-55edf892836f.gif"
+        "https://media.tenor.com/3Yt5xkK8hQAAAAAC/anime-kiss.gif",
+        "https://media.tenor.com/9Ab2cX1pLmAAAAAC/kiss-anime.gif",
+        "https://media.tenor.com/2kQx8hZy0rAAAAAC/cute-kiss.gif"
     ];
 
     const gif = gifs[Math.floor(Math.random() * gifs.length)];
