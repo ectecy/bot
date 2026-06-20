3d06802495e38c2583e0dc1ea6c4e48766514694
ectecy <velocity3343@gmail.com>
2026-06-19 20:10:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -534,14 +534,21 @@ if (cmd === "hug") {
         return message.reply("❌ Mention someone to hug.");
     }
 
-    const gif = "https://tenor.com/view/yukon-child-form-embracing-ulquiorra-gif-15599442819011505520";
+    const gifs = [
+        "https://images-ext-1.discordapp.net/external/eRYiZabajb-add7P4V4XrTJZL01or6Izr6sSd_1y7z4/https/nekos.best/api/v2/hug/c08992e3-ec9c-4cf4-88d0-6ca921d3ef64.gif",
+        "https://images-ext-1.discordapp.net/external/37qvtvvQ08qWOfXeLbWtKg7fWEKmUd9So4RYWB2_1sA/https/nekos.best/api/v2/hug/efdcc435-0402-4703-bc3b-cab18ee653bb.gif",
+        "https://images-ext-1.discordapp.net/external/EmtO43xHdJSevgT2Z_o1ii9pkIWp6xx4wiG5Zok35zY/https/nekos.best/api/v2/hug/63af9168-20da-42f8-a3f3-0299743f513c.gif",
+        "https://images-ext-1.discordapp.net/external/_cgFjM_9UiGfG4IgQRlXbKFYYmHguUWhkbZlWw9pt2s/https/nekos.best/api/v2/hug/01b8d0bb-827b-49ed-a538-c109ee5883e1.gif"
+    ];
 
-    return message.channel.send({
-        content: `🤗 ${message.author} hugs ${member.user}`,
-        embeds: [
-            new EmbedBuilder().setImage(gif)
-        ]
-    });
+    const gif = gifs[Math.floor(Math.random() * gifs.length)];
+
+    const embed = new EmbedBuilder()
+        .setColor("Pink")
+        .setDescription(`🤗 ${message.author} hugs ${member.user}`)
+        .setImage(gif);
+
+    return message.channel.send({ embeds: [embed] });
 }
         
 if (cmd === "kiss") {
@@ -550,14 +557,21 @@ if (cmd === "kiss") {
         return message.reply("❌ Mention someone to kiss.");
     }
 
-    const gif = "https://media.tenor.com/3JZ6k2oQxqAAAAAC/anime-kiss.gif";
+    const gifs = [
+        "https://images-ext-1.discordapp.net/external/0m4Oy_Ijxw91S72KgCtOk_izKq3QgYccyOFt3SxmVX8/https/nekos.best/api/v2/kiss/ec4ea38c-2433-478b-83fd-897e7c750111.gif",
+        "https://images-ext-1.discordapp.net/external/kwUZ9Gy_V9BQbjMyurp0DiC8F8E8ncBzgzcgQhOJpcw/https/nekos.best/api/v2/kiss/a24dd374-8424-4015-8d99-a0187c6debcd.gif",
+        "https://images-ext-1.discordapp.net/external/zqKWFXh7-Y2VpfyAFnnUSrl2Vc4PvrYxggRDS9XThuw/https/nekos.best/api/v2/kiss/265e12ad-eaff-4309-bcba-e7ba8173e580.gif",
+        "https://images-ext-1.discordapp.net/external/_jvUgcTCOS0-Hy6i5QKqekYCtayZRhL_a4L8yxaM4Q8/https/nekos.best/api/v2/kiss/7d248c83-2c91-480d-b7a2-55edf892836f.gif"
+    ];
 
-    return message.channel.send({
-        content: `💋 ${message.author} kisses ${member.user}`,
-        embeds: [
-            new EmbedBuilder().setImage(gif)
-        ]
-    });
+    const gif = gifs[Math.floor(Math.random() * gifs.length)];
+
+    const embed = new EmbedBuilder()
+        .setColor("Pink")
+        .setDescription(`💋 ${message.author} kisses ${member.user}`)
+        .setImage(gif);
+
+    return message.channel.send({ embeds: [embed] });
 }
         
 if (cmd === "slap") {
@@ -567,8 +581,10 @@ if (cmd === "slap") {
     }
 
     const gifs = [
-        "https://images-ext-1.discordapp.net/external/OLmWWwnlAKonK3vrZKAd505N3RpjncLVABAoz_rBBhE/https/nekos.best/api/v2/slap/facb4ca9-b269-4b88-b72d-f99416546bfb.gif",
-        "https://images-ext-1.discordapp.net/external/hAwJolPkYUKvbR1a-O2CCHGLb_GGDC8PtrnsNRev3lI/https/nekos.best/api/v2/slap/6fdd5bd2-19d1-4c6e-9f61-d6209f1625ba.gif"
+        "https://images-ext-1.discordapp.net/external/eo41PefE1eBGfQy2pCJ6NnwDDL7lXvhTVzpGk13KmhE/https/images-ext-1.discordapp.net/external/OLmWWwnlAKonK3vrZKAd505N3RpjncLVABAoz_rBBhE/https/nekos.best/api/v2/slap/facb4ca9-b269-4b88-b72d-f99416546bfb.gif",
+        "https://images-ext-1.discordapp.net/external/xx6AUAlu3HcLJZd-97rMzc4a3zvisNNRXbWsuaWIgv8/https/images-ext-1.discordapp.net/external/hAwJolPkYUKvbR1a-O2CCHGLb_GGDC8PtrnsNRev3lI/https/nekos.best/api/v2/slap/6fdd5bd2-19d1-4c6e-9f61-d6209f1625ba.gif",
+        "https://images-ext-1.discordapp.net/external/RpcknjgUWPVLSljAeCscJyqER8IgTi8-TosDVzxW5E4/https/nekos.best/api/v2/slap/0365dc87-17d5-4a4b-b04a-e1fb2c052e55.gif",
+        "https://images-ext-1.discordapp.net/external/j_FkU5x1BV7bXhmfBm4Znolav2l7mAP_UPd7e1G7JHw/https/nekos.best/api/v2/slap/86fb2280-f18d-4a01-9d7b-662209c82684.gif"
     ];
 
     const gif = gifs[Math.floor(Math.random() * gifs.length)];
@@ -589,8 +605,10 @@ if (cmd === "shoot") {
     }
 
     const gifs = [
-        "https://tenor.com/view/cowboy-bebop-faye-valentine-faye-smg-machine-gun-gif-26549733",
-        "https://tenor.com/view/anime-gun-gunslinger-girl-henrietta-gif-13064976"
+        "https://images-ext-1.discordapp.net/external/ObjpTlGgXfr13OwQKRJWumsxuLR5cS4WzBvR7_5wNc4/https/nekos.best/api/v2/shoot/55157a2c-28c1-4ffd-a0b6-7be8eb652b30.gif",
+        "https://images-ext-1.discordapp.net/external/72l3p5dF24Di2ktiCzZuf4bSUZjreuE9f2YphPxZnyo/https/nekos.best/api/v2/shoot/453ab869-f191-4555-a0ea-7a1c70013751.gif",
+        "https://images-ext-1.discordapp.net/external/seLQ81XQmitmApBjKpHgc4C6YZzmcxgD-FvDvsGe9Bw/https/nekos.best/api/v2/shoot/77daac09-d578-459d-939e-eb7f4465a16e.gif",
+        "https://images-ext-1.discordapp.net/external/jLtAR2DSMbE3RoSC_nxSci9mZqU4XIQX_zClaapxXLU/https/nekos.best/api/v2/shoot/83ed8fd5-8ad7-4375-af87-d67a63aeaaed.gif"
     ];
 
     const gif = gifs[Math.floor(Math.random() * gifs.length)];
